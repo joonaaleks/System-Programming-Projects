1497ee00b8e27a92b83adcaac75b4427da09f57b
joonaaleks <joonaaleks@gmail.com>
2022-04-23 12:53:27 +0300

    Project 4 changes and comments

--- Project1/project1.c
+++ Project1/project1.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
-        fprintf(stderr, "error: cannot open file %s\n", file);
+        fprintf(stderr, "error: cannot open file\n");
         return 1;
     }
 
